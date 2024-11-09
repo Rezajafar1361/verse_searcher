1461124804bf231ca013f8d6b3752cd13c990f75
R_J <rezajafar90@gmail.com>
2024-11-10 00:48:41 +0330

    initial

---
+++ test.py
@@ -0,0 +1,9 @@
+from src.quran_search_in_text.ayeh_yab import AyehYab
+
+file1 = open('1.txt', 'r', encoding='utf-8')
+rows = file1.readlines()
+a = AyehYab().ayeh_find(rows)
+
+print(a['html_output'])
+print(a['list_output'])
+
